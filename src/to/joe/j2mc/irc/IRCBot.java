c58061d9e52a6bf54c84ddc24d88c99271c5e279
Ammar Askar <ammar_askar@hotmail.com>
2012-02-24 22:54:31 +0500

    Command processing in private messages

--- src/to/joe/j2mc/irc/IRCBot.java
+++ src/to/joe/j2mc/irc/IRCBot.java
@@ -10,93 +10,147 @@ import to.joe.j2mc.core.event.MessageEvent;
 
 public class IRCBot extends PircBot {
 
-	J2MC_IRC plugin;
-	IRCcommands commands;
-	
-	public IRCBot(String nick, J2MC_IRC j2mc_irc) {
-		this.setName(nick);
-		this.setAutoNickChange(true);
-		this.setMessageDelay(1100);
-		this.plugin = j2mc_irc;
-		this.commands = new IRCcommands(this, plugin);
-	}
+    J2MC_IRC plugin;
+    IRCcommands commands;
 
-	//COmmands begin here.
-	public void onMessage(String channel, String sender, String login, String hostname, String message) {
-		//All channel commands.
-	    String[] MessageArray = message.split(" ");
-		
-		//!playerlist command
-		if(message.toLowerCase().equalsIgnoreCase("!playerlist")){
-		    commands.PlayerListCommand(channel);
-		}
-		
-		//!players command
-		if(message.toLowerCase().equalsIgnoreCase("!players")){
-		    commands.PlayersCommand(channel);
-		}
-		
-		//Public channel commands.
-		if(channel.equalsIgnoreCase(plugin.NormalChannel)){
-			//!msg command
-			if(MessageArray[0].equalsIgnoreCase("!msg")) {
-				String toSend = message.substring(5);
-				String toSendIRC = "[IRC] <" + sender + "> " + toSend;
-				toSend = "(IRC) <" + ChatColor.AQUA + sender + ChatColor.WHITE + "> " + toSend;
-				plugin.getServer().broadcastMessage(toSend);
-				this.sendMessage(channel, toSendIRC);
-			}
-			
-			//.kick command
-			if(MessageArray[0].equalsIgnoreCase(".kick")){
-			    if(commands.hasAdminPrivelages(hostname)){
-			        String[] args = message.substring(6).split(" ");
-			        String partialplayer = args[0];  
-			        String reason = J2MC_Core.combineSplit(1, args, " ");
-			        commands.dotKickCommand(partialplayer, reason, sender, hostname);
-			    }
-			}
-			
-			//.g command
-			if(MessageArray[0].equalsIgnoreCase(".g")){
-			    if(commands.hasAdminPrivelages(hostname)){
-			        String gmessage = message.substring(3);
-			        commands.dotGCommand(hostname, gmessage);
-			    }
-			}
-			
-			//!admins command
-			if(MessageArray[0].equalsIgnoreCase("!admins")){
-			    commands.AdminsCommandinPublic(channel);
-			}
-			
-			//.a command
-			if(MessageArray[0].equalsIgnoreCase(".a")){
-			    String Derp = message.substring(3);
-			    commands.dotAcommand(hostname, Derp);
-			}
-		}
-		
-		//Admin chanel commands.
-		if(channel.equalsIgnoreCase(plugin.AdminChannel)){
-		    //!has command
-		    if(MessageArray[0].equalsIgnoreCase("!has")){
-		        String player = MessageArray[1];
-		        commands.HasCommand(player, channel);
-		    }
-		    
-		    //!admins command
-		    if(MessageArray[0].equalsIgnoreCase("!admins")){
-		        commands.AdminsCommandinPrivate(channel);
-		    }
-		    
-		    //!reports command
-		    if(message.equalsIgnoreCase("!reports")){
-	            HashSet<String> targets = new HashSet<String>();
-	            targets.add("ReportCall");
-	            plugin.getServer().getPluginManager().callEvent(new MessageEvent(targets, "Request for reports. Message useless :3"));
-		    }
-		}
-		
-	}
+    public IRCBot(String nick, J2MC_IRC j2mc_irc) {
+        this.setName(nick);
+        this.setAutoNickChange(true);
+        this.setMessageDelay(1100);
+        this.plugin = j2mc_irc;
+        this.commands = new IRCcommands(this, plugin);
+    }
+
+    // COmmands begin here.
+    public void onMessage(String channel, String sender, String login, String hostname, String message) {
+        // All channel commands.
+        String[] MessageArray = message.split(" ");
+
+        // !playerlist command
+        if (message.toLowerCase().equalsIgnoreCase("!playerlist")) {
+            commands.PlayerListCommand(channel);
+        }
+
+        // !players command
+        if (message.toLowerCase().equalsIgnoreCase("!players")) {
+            commands.PlayersCommand(channel);
+        }
+
+        // Public channel commands.
+        if (channel.equalsIgnoreCase(plugin.NormalChannel)) {
+            // !msg command
+            if (MessageArray[0].equalsIgnoreCase("!msg")) {
+                String toSend = message.substring(5);
+                String toSendIRC = "[IRC] <" + sender + "> " + toSend;
+                toSend = "(IRC) <" + ChatColor.AQUA + sender + ChatColor.WHITE + "> " + toSend;
+                plugin.getServer().broadcastMessage(toSend);
+                this.sendMessage(channel, toSendIRC);
+            }
+
+            // .kick command
+            if (MessageArray[0].equalsIgnoreCase(".kick")) {
+                if (commands.hasAdminPrivelages(hostname)) {
+                    String[] args = message.substring(6).split(" ");
+                    String partialplayer = args[0];
+                    String reason = J2MC_Core.combineSplit(1, args, " ");
+                    commands.dotKickCommand(partialplayer, reason, sender, hostname);
+                }
+            }
+
+            // .g command
+            if (MessageArray[0].equalsIgnoreCase(".g")) {
+                if (commands.hasAdminPrivelages(hostname)) {
+                    String gmessage = message.substring(3);
+                    commands.dotGCommand(hostname, gmessage);
+                }
+            }
+
+            // !admins command
+            if (MessageArray[0].equalsIgnoreCase("!admins")) {
+                commands.AdminsCommandinPublic(channel);
+            }
+
+            // .a command
+            if (MessageArray[0].equalsIgnoreCase(".a")) {
+                if (commands.hasAdminPrivelages(hostname)) {
+                    String Derp = message.substring(3);
+                    commands.dotAcommand(hostname, Derp);
+                }
+            }
+        }
+
+        // Admin chanel commands.
+        if (channel.equalsIgnoreCase(plugin.AdminChannel)) {
+            // !has command
+            if (MessageArray[0].equalsIgnoreCase("!has")) {
+                String player = MessageArray[1];
+                commands.HasCommand(player, channel);
+            }
+
+            // !admins command
+            if (MessageArray[0].equalsIgnoreCase("!admins")) {
+                commands.AdminsCommandinPrivate(channel);
+            }
+
+            // !reports command
+            if (message.equalsIgnoreCase("!reports")) {
+                HashSet<String> targets = new HashSet<String>();
+                targets.add("ReportCall");
+                plugin.getServer().getPluginManager().callEvent(new MessageEvent(targets, "Request for reports. Message useless :3"));
+            }
+        }
+
+    }
+
+    @Override
+    protected void onPrivateMessage(String sender, String login, String hostname, String message) {
+        String[] MessageArray = message.split(" ");
+        // playerlist command
+        if (message.toLowerCase().equalsIgnoreCase("playerlist")) {
+            commands.PlayerListCommand(sender);
+        }
+        // players command
+        if (message.toLowerCase().equalsIgnoreCase("players")) {
+            commands.PlayersCommand(sender);
+        }
+        // !msg command
+        if (MessageArray[0].equalsIgnoreCase("msg")) {
+            String toSend = message.substring(4);
+            String toSendIRC = "[IRC] <" + sender + "> " + toSend;
+            toSend = "(IRC) <" + ChatColor.AQUA + sender + ChatColor.WHITE + "> " + toSend;
+            plugin.getServer().broadcastMessage(toSend);
+            this.sendMessage(plugin.NormalChannel, toSendIRC);
+        }
+        // kick command
+        if (MessageArray[0].equalsIgnoreCase("kick")) {
+            if (commands.hasAdminPrivelages(hostname)) {
+                String[] args = message.substring(5).split(" ");
+                String partialplayer = args[0];
+                String reason = J2MC_Core.combineSplit(1, args, " ");
+                commands.dotKickCommand(partialplayer, reason, sender, hostname);
+            }
+        }
+        // g command
+        if (MessageArray[0].equalsIgnoreCase("g")) {
+            if (commands.hasAdminPrivelages(hostname)) {
+                String gmessage = message.substring(2);
+                commands.dotGCommand(hostname, gmessage);
+            }
+        }
+        // a command
+        if (MessageArray[0].equalsIgnoreCase("a")) {
+            if (commands.hasAdminPrivelages(hostname)) {
+                String Derp = message.substring(2);
+                commands.dotAcommand(hostname, Derp);
+            }
+        }
+        // admins command
+        if (MessageArray[0].equalsIgnoreCase("admins")) {
+            if (commands.hasAdminPrivelages(hostname)) {
+                commands.AdminsCommandinPrivate(sender);
+            }else{
+                commands.AdminsCommandinPublic(sender);
+            }
+        }
+    }
 }

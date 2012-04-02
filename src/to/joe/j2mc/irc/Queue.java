1fe5ca9daf0545220fb9a326beb9f7d98cb303b6
Ammar Askar <ammar_askar@hotmail.com>
2012-04-02 21:00:52 +0500

    Limit same message sending to 5 per 10 seconds

--- src/to/joe/j2mc/irc/Queue.java
+++ src/to/joe/j2mc/irc/Queue.java
@@ -1,11 +1,14 @@
 package to.joe.j2mc.irc;
 
+import java.util.ArrayList;
 import java.util.TimerTask;
 
 public class Queue extends TimerTask {
 
     J2MC_IRC plugin;
     int OutGoingMessages = 0;
+    ArrayList<String> messages = new ArrayList<String>();
+    int SimilarMessagesOut = 0;
 
     public Queue(J2MC_IRC plugin) {
         this.plugin = plugin;
@@ -14,19 +17,27 @@ public class Queue extends TimerTask {
     @Override
     public void run() {
         this.OutGoingMessages = 0;
+        this.SimilarMessagesOut = 0;
+        this.messages.clear();
     }
 
     public void sendMessage(String message, String channel) {
         synchronized (this) {
             this.OutGoingMessages++;
         }
-        if ((this.OutGoingMessages >= 10) && (this.OutGoingMessages < 20)) {
+        if(this.messages.get(this.messages.size()).equals(message)){
+            synchronized (this) {
+                this.SimilarMessagesOut++;
+            }
+        }
+        this.messages.add(message);
+        if ((this.OutGoingMessages >= 10) && (this.OutGoingMessages < 20) && (this.SimilarMessagesOut <= 5)) {
             try {
                 Thread.sleep(1000);
             } catch (final InterruptedException e) {
             }
             this.plugin.IRCManager.bot.sendMessage(channel, message);
-        } else if (this.OutGoingMessages >= 20) {
+        } else if ((this.OutGoingMessages >= 20) && (this.SimilarMessagesOut <= 5)) {
             try {
                 Thread.sleep(2000);
             } catch (final InterruptedException e) {

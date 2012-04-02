079ddc1ff784aec8239573010897a78f123ee781
Ammar Askar <ammar_askar@hotmail.com>
2012-04-02 21:36:13 +0500

    Fixes stuff

--- src/to/joe/j2mc/irc/Queue.java
+++ src/to/joe/j2mc/irc/Queue.java
@@ -25,25 +25,29 @@ public class Queue extends TimerTask {
         synchronized (this) {
             this.OutGoingMessages++;
         }
-        if(this.messages.get(this.messages.size() - 1).equals(message)){
-            synchronized (this) {
-                this.SimilarMessagesOut++;
+        if (!this.messages.isEmpty()) {
+            if ((this.messages.get(this.messages.size() - 1)).equals(message)) {
+                synchronized (this) {
+                    this.SimilarMessagesOut++;
+                }
             }
         }
         this.messages.add(message);
-        if ((this.OutGoingMessages >= 10) && (this.OutGoingMessages < 20) && (this.SimilarMessagesOut <= 5)) {
+        if (this.SimilarMessagesOut <= 7){
+            return;
+        }
+        if ((this.OutGoingMessages >= 10) && (this.OutGoingMessages < 20)) {
             try {
                 Thread.sleep(1000);
             } catch (final InterruptedException e) {
             }
             this.plugin.IRCManager.bot.sendMessage(channel, message);
-        } else if ((this.OutGoingMessages >= 20) && (this.SimilarMessagesOut <= 5)) {
+        } else if ((this.OutGoingMessages >= 20)) {
             try {
                 Thread.sleep(2000);
             } catch (final InterruptedException e) {
             }
             this.plugin.IRCManager.bot.sendMessage(channel, message);
-
         } else {
             this.plugin.IRCManager.bot.sendMessage(channel, message);
         }
